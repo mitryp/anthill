55b5ca3ff001633ab4bd6f64f5cac6f017629087
Dmytro Popov <thedmitryp@ukr.net>
2023-11-12 01:26:59 +0000

    Added the `serve-static` module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('lifespan')
   getLifespan(): string {
     return `The app is active for ${this.appService.getCurrentLifespanSec()} seconds.`;
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigurationModule } from './common/configuration/configuration.module';
 import { ConfigurationHttpService } from './common/configuration/configuration.http.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [ConfigurationModule],
+  imports: [
+    ConfigurationModule,
+    ServeStaticModule.forRootAsync({
+      imports: [ConfigurationModule],
+      inject: [ConfigurationHttpService],
+      useFactory: (httpConfig: ConfigurationHttpService) => [
+        {
+          rootPath: httpConfig.staticPath,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ConfigurationHttpService],
 })
