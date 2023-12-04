4e1cd48044fe445d8910a8ad5d2472abc2e74378
Dmytro Popov <thedmitryp@ukr.net>
2023-12-04 17:11:57 +0000

    Added Swagger endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigurationHttpService } from './common/configuration/configuration.http.service';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const env = process.env.NODE_ENV;
+  if (!env || env === 'development') {
+    setupSwaggerApi(app);
+  }
 
   const { port, host, staticPath } = app.get(ConfigurationHttpService);
 
   console.log(host, port, staticPath);
 
   await app.listen(port, host, () => console.log(`Listening on http://${host}:${port}`));
+}
+
+function setupSwaggerApi(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Anthill API')
+    .setDescription('The API for interacting with Anthill backend')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 }
 
 bootstrap();
