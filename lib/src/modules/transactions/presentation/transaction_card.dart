0333b439f307cb1745ad0012ef69ed4c34c4e518
Dmytro Popov <thedmitryp@ukr.net>
2023-12-07 19:21:30 +0000

    Temporarily implemented `Transaction`-related widgets

--- lib/src/modules/transactions/presentation/transaction_card.dart
+++ lib/src/modules/transactions/presentation/transaction_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../domain/dtos/transaction_read_dto.dart';
+import 'transaction_view.dart';
 
 class TransactionCard extends StatelessWidget {
   final TransactionReadDto _transaction;
@@ -15,6 +16,9 @@ class TransactionCard extends StatelessWidget {
         title: Text(_transaction.sourceOrPurpose),
         subtitle: _transaction.note.isNotEmpty ? Text(_transaction.note) : null,
         trailing: Text('${_transaction.amount}GBP'),
+        onTap: () => Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => TransactionView(transaction: _transaction)),
+        ),
       ),
     );
   }
--- lib/src/modules/transactions/presentation/transaction_view.dart
+++ lib/src/modules/transactions/presentation/transaction_view.dart
@@ -3,10 +3,77 @@ import 'package:flutter/material.dart';
 import '../domain/dtos/transaction_read_dto.dart';
 
 class TransactionView extends StatelessWidget {
-  const TransactionView({super.key});
+  final TransactionReadDto _transaction;
+
+  const TransactionView({required TransactionReadDto transaction, super.key})
+      : _transaction = transaction;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final col = Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Card(
+          child: ListTile(
+            title: Text('Transaction: ${_transaction.sourceOrPurpose}'),
+            subtitle: _transaction.note.isNotEmpty ? Text(_transaction.note) : null,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(4),
+          child: OverflowBar(
+            alignment: MainAxisAlignment.start,
+            spacing: 8,
+            overflowSpacing: 8,
+            children: [
+              Chip(
+                label: Text('${_transaction.amount}'),
+                backgroundColor: _transaction.isIncome ? Colors.green[300] : Colors.redAccent[100],
+              ),
+              Chip(label: Text('${_transaction.createDate}')),
+              if (_transaction.deleteDate != null) Chip(label: Text('${_transaction.deleteDate}')),
+            ],
+          ),
+        ),
+      ],
+    );
+
+    final controls = OverflowBar(
+      alignment: MainAxisAlignment.spaceAround,
+      children: [
+        OutlinedButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.delete),
+          label: const Text('Delete'),
+          style: ButtonStyle(foregroundColor: MaterialStatePropertyAll(Colors.red[400])),
+        ),
+        ElevatedButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.edit),
+          label: const Text('Edit'),
+        ),
+      ],
+    );
+
+    final child = col;
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: LayoutBuilder(
+          builder: (context, constraints) => ConstrainedBox(
+            constraints: constraints.widthConstraints() / 2.5,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                child,
+                const SizedBox(height: 32),
+                controls,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
