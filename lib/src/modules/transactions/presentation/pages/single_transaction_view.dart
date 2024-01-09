1dc3bc9e51be9ffc1153a89cbb75f5e55ab98abd
Dmytro Popov <77672371+mitryp@users.noreply.github.com>
2024-01-09 00:06:28 +0000

    Renamed `TransactionView` to `SingleTransactionView`

--- lib/src/modules/transactions/presentation/pages/single_transaction_view.dart
+++ lib/src/modules/transactions/presentation/pages/single_transaction_view.dart
@@ -13,15 +13,16 @@ import '../../application/providers/transaction_controller_provider.dart';
 import '../../application/providers/transaction_by_id_provider.dart';
 import '../../domain/dtos/transaction_read_dto.dart';
 
-class TransactionView extends ConsumerWidget {
+class SingleTransactionView extends ConsumerWidget {
   final int _transactionId;
   final TransactionReadDto? _transaction;
 
-  const TransactionView({required int transactionId, TransactionReadDto? transaction, super.key})
+  const SingleTransactionView(
+      {required int transactionId, TransactionReadDto? transaction, super.key})
       : _transactionId = transactionId,
         _transaction = transaction;
 
-  factory TransactionView.pageBuilder(BuildContext context, GoRouterState state) {
+  factory SingleTransactionView.pageBuilder(BuildContext context, GoRouterState state) {
     final idStr = state.pathParameters['id'];
     final extra = state.extra;
     final passedTransaction = extra is TransactionReadDto ? extra : null;
@@ -31,7 +32,7 @@ class TransactionView extends ConsumerWidget {
       throw StateError('transaction id was not correct');
     }
 
-    return TransactionView(
+    return SingleTransactionView(
       transactionId: id,
       transaction: passedTransaction,
     );
--- lib/src/shared/presentation/constraints/app_page.dart
+++ lib/src/shared/presentation/constraints/app_page.dart
@@ -1,5 +1,5 @@
 import '../../../modules/transactions/presentation/pages/transactions_page.dart';
-import '../../../modules/transactions/presentation/pages/transaction_view.dart';
+import '../../../modules/transactions/presentation/pages/single_transaction_view.dart';
 import '../../../modules/transactions/presentation/transaction_editor.dart';
 import '../../typedefs.dart';
 
@@ -9,7 +9,7 @@ const defaultPage = AppPage.transactions;
 enum AppPage {
   transactions('/transactions', TransactionsPage.pageBuilder),
   transactionEditor('/transactions/editor', TransactionEditor.pageBuilder),
-  transaction('/transactions/$idParamPlaceholder', TransactionView.pageBuilder);
+  transaction('/transactions/$idParamPlaceholder', SingleTransactionView.pageBuilder);
 
   final String location;
   final PageBuilder pageBuilder;
