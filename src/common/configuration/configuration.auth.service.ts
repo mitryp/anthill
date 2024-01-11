0af34fad34fb5e54d0dae5d4f87315f05a30a536
Dmytro Popov <thedmitryp@ukr.net>
2024-01-11 20:45:03 +0000

    Implemented `ConfigurationAuthService`

---
+++ src/common/configuration/configuration.auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ConfigurationBaseService } from './configuration.base.service';
+
+@Injectable()
+export class ConfigurationAuthService extends ConfigurationBaseService {
+  constructor(config: ConfigService) {
+    super(config);
+  }
+
+  get configSection(): string {
+    return 'auth';
+  }
+
+  get saltRounds() {
+    return Number(this.get<number>('saltRounds') || 10);
+  }
+}
