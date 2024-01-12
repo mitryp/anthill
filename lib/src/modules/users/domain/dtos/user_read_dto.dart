2b6e7a2fd4c355614f3d18bad7e42327a966df36
Dmytro Popov <thedmitryp@ukr.net>
2024-01-12 15:24:13 +0000

    Implemented users module DTOs

---
+++ lib/src/modules/users/domain/dtos/user_read_dto.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../../shared/domain/interfaces/model.dart';
+import '../constraints/user_role.dart';
+
+part 'user_read_dto.freezed.dart';
+
+part 'user_read_dto.g.dart';
+
+@freezed
+class UserReadDto with _$UserReadDto implements Model {
+  const factory UserReadDto({
+    required int id,
+    required DateTime createDate,
+    DateTime? deleteDate,
+    required String name,
+    required String email,
+    required UserRole role,
+  }) = _UserReadDto;
+
+  factory UserReadDto.fromJson(Map<String, Object?> json) => _$UserReadDtoFromJson(json);
+}
---
+++ lib/src/modules/users/users_module.dart
@@ -0,0 +1,3 @@
+export 'domain/constraints/user_role.dart' show UserRole;
+export 'domain/dtos/user_create_dto.dart' show UserCreateDto;
+export 'domain/dtos/user_read_dto.dart' show UserReadDto;
