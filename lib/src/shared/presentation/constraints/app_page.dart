7d5f8271a71a766edd62ede612a25b29baef3699
Dmytro Popov <thedmitryp@ukr.net>
2024-01-12 19:09:51 +0000

    Added a route for the single user page

--- lib/src/shared/presentation/constraints/app_page.dart
+++ lib/src/shared/presentation/constraints/app_page.dart
@@ -1,13 +1,18 @@
 import '../../../modules/transactions/transactions_module.dart';
+import '../../../modules/users/presentation/pages/single_user_page.dart';
 import '../../typedefs.dart';
 
 const idParamPlaceholder = ':id';
 const defaultPage = AppPage.transactions;
 
+// todo invent a way to register routes for modules dynamically
+
 enum AppPage {
   transactions('/transactions', TransactionsPage.pageBuilder),
   transactionEditor('/transactions/editor', TransactionEditor.pageBuilder),
-  transaction('/transactions/$idParamPlaceholder', SingleTransactionView.pageBuilder);
+  transaction('/transactions/$idParamPlaceholder', SingleTransactionView.pageBuilder),
+  user('/users/$idParamPlaceholder', SingleUserPage.pageBuilder),
+  ;
 
   final String location;
   final PageBuilder pageBuilder;
