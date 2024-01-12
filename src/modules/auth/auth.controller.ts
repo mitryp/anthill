fbb370c9d409fba302fed09021c0fb01e316c563
Dmytro Popov <thedmitryp@ukr.net>
2024-01-12 12:53:56 +0000

    Fixed Swagger docs for the `login` endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { Request, Response } from 'express';
 import { LocalAuthGuard } from './local-auth.guard';
@@ -6,7 +6,10 @@ import { User } from '../users/data/entities/user.entity';
 import { Public } from './public.guard';
 import { UserReadDto } from '../users/data/dtos/user.read.dto';
 import { ConfigurationAuthService } from '../../common/configuration/configuration.auth.service';
+import { ApiTags } from '@nestjs/swagger';
+import { LoginDto } from './data/dtos/login.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -17,7 +20,11 @@ export class AuthController {
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Req() req: Request, @Res() res: Response): Promise<UserReadDto> {
+  async login(
+    @Body() loginDto: LoginDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<UserReadDto> {
     const successDto = await this.authService.login(req.user as User);
 
     res
@@ -29,6 +36,6 @@ export class AuthController {
       })
       .json(successDto.user);
 
-    return;
+    return successDto.user;
   }
 }
