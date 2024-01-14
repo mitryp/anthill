d10d9f90d3225e5b318e8c0d6ecafcd0049e6335
Dmytro Popov <thedmitryp@ukr.net>
2024-01-14 18:16:27 +0000

    Fixed credentials not saving

--- anthill_front/lib/src/shared/utils/http_client.dart
+++ anthill_front/lib/src/shared/utils/http_client.dart
@@ -1,5 +1,9 @@
 import 'package:dio/dio.dart';
 
+// ignore: implementation_imports
+import 'package:dio/src/adapters/browser_adapter.dart';
+
 import '../../config.dart';
 
-Dio client() => Dio(BaseOptions()..baseUrl = apiBaseUrl);
+Dio client() => Dio(BaseOptions()..baseUrl = apiBaseUrl)
+  ..httpClientAdapter = BrowserHttpClientAdapter(withCredentials: true);
