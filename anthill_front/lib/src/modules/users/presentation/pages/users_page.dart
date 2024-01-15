5ff15f0273a7b8929c128c338da200d30768f715
Dmytro Popov <thedmitryp@ukr.net>
2024-01-15 01:06:19 +0000

    Compacted `shared_pagination`

--- anthill_front/lib/src/modules/users/presentation/pages/users_page.dart
+++ anthill_front/lib/src/modules/users/presentation/pages/users_page.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_nestjs_paginate/flutter_nestjs_paginate.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../../shared/pagination.dart';
 import '../../../../shared/presentation/widgets/copy_link_button.dart';
-import '../../../../shared/utils/normalize_query_params.dart';
-import '../../../../shared/presentation/widgets/paginated_collection_view.dart';
 import '../../application/providers/user_service_provider.dart';
 import '../../application/providers/users_provider.dart';
 import '../../domain/dtos/user_read_dto.dart';
