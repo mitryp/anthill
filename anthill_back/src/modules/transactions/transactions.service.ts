7b710864936dfc465259b887e847d40e64f3dca7
Dmytro Popov <thedmitryp@ukr.net>
2024-01-15 16:22:38 +0000

    Added user refs to the transactions entites

--- anthill_back/src/modules/transactions/transactions.service.ts
+++ anthill_back/src/modules/transactions/transactions.service.ts
@@ -5,16 +5,17 @@ import { Transaction } from './data/entities/transaction.entity';
 import { TransactionReadDto } from './data/dtos/transaction.read.dto';
 import { Repository } from 'typeorm';
 import { Mapper } from 'automapper-core';
-import { TransactionCreateDto } from './data/dtos/transaction.create.dto';
+import { TransactionCreateDtoWithUser } from './data/dtos/transaction.create.dto';
 import { TransactionUpdateDto } from './data/dtos/transaction.update.dto';
 import { ModifiableResourceServiceBase } from '../../common/domain/resource.service.base';
-import { FilterOperator, PaginateConfig } from 'nestjs-paginate';
+import { paginate, PaginateConfig, PaginateQuery } from 'nestjs-paginate';
+import { ReadManyDto } from '../../common/domain/read-many.dto';
 
 @Injectable()
 export class TransactionsService extends ModifiableResourceServiceBase<
   Transaction,
   TransactionReadDto,
-  TransactionCreateDto,
+  TransactionCreateDtoWithUser,
   TransactionUpdateDto
 > {
   constructor(
@@ -26,14 +27,25 @@ export class TransactionsService extends ModifiableResourceServiceBase<
       mapper,
       Transaction,
       TransactionReadDto,
-      TransactionCreateDto,
+      TransactionCreateDtoWithUser,
       TransactionUpdateDto,
       transactionsPaginateConfig,
     );
   }
+
+  override async readAll(query: PaginateQuery): Promise<ReadManyDto<TransactionReadDto>> {
+    // to load soft-deleted users
+    const queryBuilder = this.repository.createQueryBuilder('transaction').withDeleted();
+
+    const paginated = await paginate(query, queryBuilder, this.paginateConfig);
+
+    return this.mapPaginated(paginated);
+  }
 }
 
 export const transactionsPaginateConfig: PaginateConfig<Transaction> = {
+  withDeleted: true,
+  relations: { user: true },
   sortableColumns: ['createDate', 'amount', 'sourceOrPurpose'],
   defaultSortBy: [['createDate', 'DESC']],
   searchableColumns: ['sourceOrPurpose', 'note'],
