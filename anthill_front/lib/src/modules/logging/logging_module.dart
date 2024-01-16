497392c3ddf24acb996ef36b1cb3e6336d73430a
Dmytro Popov <thedmitryp@ukr.net>
2024-01-16 19:15:58 +0000

    Implemented logs providers

--- anthill_front/lib/src/modules/logging/logging_module.dart
+++ anthill_front/lib/src/modules/logging/logging_module.dart
@@ -1,3 +1,5 @@
+export 'application/providers/log_entry_by_id_provider.dart' show logEntryById;
 export 'application/providers/logging_service_provider.dart' show loggingServiceProvider;
+export 'application/providers/logs_provider.dart' show logsProvider;
 export 'application/services/logging_service.dart' show LoggingService;
 export 'domain/dtos/log_entry_read_dto.dart' show LogEntryReadDto;
