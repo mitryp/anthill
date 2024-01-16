9512f4b145afd3795a1377674f8ea1b36b618c27
Dmytro Popov <thedmitryp@ukr.net>
2024-01-16 19:23:29 +0000

    Added missing fields to `LogEntryReadDto`

--- anthill_front/lib/src/modules/logging/domain/dtos/log_entry_read_dto.dart
+++ anthill_front/lib/src/modules/logging/domain/dtos/log_entry_read_dto.dart
@@ -1,13 +1,16 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../../../shared/http.dart';
 import '../../../users/domain/dtos/user_read_dto.dart';
 
 part 'log_entry_read_dto.freezed.dart';
 part 'log_entry_read_dto.g.dart';
 
 @freezed
-class LogEntryReadDto with _$LogEntryReadDto {
+class LogEntryReadDto with _$LogEntryReadDto implements IdentifiableModel {
   const factory LogEntryReadDto({
+    required int id,
+    required DateTime createDate,
     required UserReadDto user,
     required String action,
     required String moduleName,
@@ -16,5 +19,11 @@ class LogEntryReadDto with _$LogEntryReadDto {
     Map<String, dynamic>? jsonPayload,
   }) = _LogEntryReadDto;
 
+  const LogEntryReadDto._();
+
   factory LogEntryReadDto.fromJson(Map<String, Object?> json) => _$LogEntryReadDtoFromJson(json);
+
+  // logs are never deleted
+  @override
+  DateTime? get deleteDate => null;
 }
