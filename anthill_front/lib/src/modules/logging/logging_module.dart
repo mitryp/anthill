9ac0ac36b8fab149a9b89d5d499d7711ace33327
Dmytro Popov <thedmitryp@ukr.net>
2024-01-16 18:58:20 +0000

    Exported application layer of `LoggingModule`

---
+++ anthill_front/lib/src/modules/logging/logging_module.dart
@@ -0,0 +1,3 @@
+export 'application/providers/logging_service_provider.dart' show loggingServiceProvider;
+export 'application/services/logging_service.dart' show LoggingService;
+export 'domain/dtos/log_entry_read_dto.dart' show LogEntryReadDto;
