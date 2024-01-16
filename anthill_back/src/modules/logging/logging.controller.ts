fca0a0ba72d875ddd9f1ede4b6fb9cfd64c20ecb
Dmytro Popov <thedmitryp@ukr.net>
2024-01-16 17:01:56 +0000

    Added `readOne` to the `LoggingController`

--- anthill_back/src/modules/logging/logging.controller.ts
+++ anthill_back/src/modules/logging/logging.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { Paginate, PaginateQuery } from 'nestjs-paginate';
 import { ReadManyDto } from '../../common/domain/read-many.dto';
 import { LogEntryReadDto } from './data/dtos/log-entry.read.dto';
@@ -13,5 +13,16 @@ export class LoggingController {
   @Get()
   async readAll(@Paginate() query: PaginateQuery): Promise<ReadManyDto<LogEntryReadDto>> {
     return this.loggingService.readAll(query);
+  }
+
+  @Get(':id')
+  async readOne(@Param('id') id: number) {
+    const entry = await this.loggingService.readOne(id);
+
+    if (!entry) {
+      throw new NotFoundException();
+    }
+
+    return entry;
   }
 }
