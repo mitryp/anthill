8d0ea33baacf16138a5d8b8799676a763b851bdc
Dmytro Popov <thedmitryp@ukr.net>
2024-02-16 17:39:29 +0000

    Updated `AuthController` to use sessions

--- anthill_back/src/modules/auth/auth.controller.ts
+++ anthill_back/src/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
-import { AuthenticationService } from './authentication.service';
-import { Request, Response } from 'express';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Request } from 'express';
 import { LocalAuthGuard } from './local-auth.guard';
 import { User } from '../users/data/entities/user.entity';
 import { Public } from './public.guard';
@@ -8,40 +8,28 @@ import { UserReadDto } from '../users/data/dtos/user.read.dto';
 import { ConfigurationAuthService } from '../../common/configuration/configuration.auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { LoginDto } from './data/dtos/login.dto';
-import { JwtPayloadDto } from './data/dtos/jwt.payload.dto';
+import { SessionPayloadDto } from './data/dtos/session.payload.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authConfig: ConfigurationAuthService,
-    private readonly authService: AuthenticationService,
+    private readonly authService: AuthService,
   ) {}
 
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(
-    @Body() loginDto: LoginDto,
+    @Body() _loginDto: LoginDto,
     @Req() req: Request,
-    @Res() res: Response,
   ): Promise<object> {
-    const successDto = await this.authService.login(req.user as User);
-
-    res
-      .cookie('accessToken', successDto.accessToken, {
-        httpOnly: true,
-        secure: this.authConfig.useSecureCookies,
-        sameSite: 'strict',
-        maxAge: this.authConfig.sessionTtl,
-      })
-      .send();
-
-    return {};
+    return await this.authService.login(req.user as User);
   }
 
   @Post('restore')
   async restoreSession(@Req() req: Request): Promise<UserReadDto> {
-    return this.authService.restoreUser(req.user as JwtPayloadDto);
+    return this.authService.restoreUser(req.user as SessionPayloadDto);
   }
 }
