63d883b926142d702bb63b8df333f4587f4fd36e
Dmytro Popov <thedmitryp@ukr.net>
2024-02-16 17:39:56 +0000

    Updated `AuthModule` configuration

--- anthill_back/src/modules/auth/auth.module.ts
+++ anthill_back/src/modules/auth/auth.module.ts
@@ -1,37 +1,26 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { AuthenticationService } from './authentication.service';
+import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/data/entities/user.entity';
 import { ConfigurationModule } from '../../common/configuration/configuration.module';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigurationAuthService } from '../../common/configuration/configuration.auth.service';
 import { UsersModule } from '../users/users.module';
-import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
-import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
 import { EncryptionService } from '../../common/utils/encryption.service';
+import { Session } from './data/entities/session.entity';
+import { AuthMapper } from './data/auth.mapper';
+import { AccountSerializer } from './account.serializer';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     forwardRef(() => UsersModule),
-    PassportModule,
-    TypeOrmModule.forFeature([User]),
-    JwtModule.registerAsync({
-      global: true,
-      imports: [ConfigurationModule],
-      inject: [ConfigurationAuthService],
-      useFactory: (authConfig: ConfigurationAuthService) => ({
-        secret: authConfig.sessionSecret,
-        signOptions: {
-          expiresIn: authConfig.sessionTtl,
-        },
-      }),
-    }),
+    PassportModule.register({ session: true }),
+    TypeOrmModule.forFeature([User, Session]),
     ConfigurationModule,
   ],
   controllers: [AuthController],
-  providers: [AuthenticationService, LocalStrategy, JwtStrategy, EncryptionService],
-  exports: [AuthenticationService, EncryptionService],
+  providers: [AuthService, LocalStrategy, EncryptionService, AuthMapper, AccountSerializer],
+  exports: [AuthService, EncryptionService],
 })
 export class AuthModule {}
