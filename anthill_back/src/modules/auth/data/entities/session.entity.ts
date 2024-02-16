c40593d22fa0b3f3b48281098986ffd9ae116694
Dmytro Popov <thedmitryp@ukr.net>
2024-02-16 17:26:01 +0000

    Implemented `Session` entity

---
+++ anthill_back/src/modules/auth/data/entities/session.entity.ts
@@ -0,0 +1,18 @@
+import { ISession } from 'connect-typeorm';
+import { Column, DeleteDateColumn, Entity, Index, PrimaryColumn } from 'typeorm';
+
+@Entity('sessions')
+export class Session implements ISession {
+  @Index()
+  @Column('bigint')
+  expiredAt = Date.now();
+
+  @PrimaryColumn('varchar', { length: 255 })
+  id = '';
+
+  @Column('text')
+  json = '';
+
+  @DeleteDateColumn()
+  destroyedAt?: Date;
+}
