0c184aca2061f37fa3faade95e29e60e619e2900
Dmytro Popov <thedmitryp@ukr.net>
2024-03-17 00:55:31 +0000

    Added stats fetch method to `TransactionService`

--- anthill_front/lib/src/modules/transactions/application/services/transaction_service.dart
+++ anthill_front/lib/src/modules/transactions/application/services/transaction_service.dart
@@ -1,6 +1,9 @@
 import '../../../../shared/http.dart';
+import '../../../../shared/typedefs.dart';
+import '../../../../shared/utils/date_transfer_format.dart';
 import '../../domain/dtos/transaction_create_dto.dart';
 import '../../domain/dtos/transaction_read_dto.dart';
+import '../../domain/dtos/transaction_stats_dto.dart';
 
 const transactionsResourceName = 'transactions';
 
@@ -8,4 +11,23 @@ class TransactionService extends HttpService<TransactionReadDto>
     with HttpWriteMixin<TransactionReadDto, TransactionCreateDto, TransactionCreateDto> {
   const TransactionService({required super.client})
       : super(decoder: TransactionReadDto.fromJson, apiPrefix: transactionsResourceName);
+
+  Future<TransactionStatsDto> getStats({
+    required DateTime fromDate,
+    required DateTime toDate,
+  }) async {
+    final data = await client.get<JsonMap>(
+      '$apiPrefix/stats/range',
+      queryParameters: {
+        'from': serializeDateQueryParam(fromDate),
+        'to': serializeDateQueryParam(toDate),
+      },
+    ).then((res) => res.data);
+
+    if (data == null) {
+      throw NoResourceError('TransactionStatsDto');
+    }
+
+    return TransactionStatsDto.fromJson(data);
+  }
 }
