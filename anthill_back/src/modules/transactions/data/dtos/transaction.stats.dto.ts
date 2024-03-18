f8424f900d15f36f27566ccd15edbe884a59b3aa
Dmytro Popov <thedmitryp@ukr.net>
2024-03-18 23:57:02 +0000

    Extended `TransactionStatsDto`

--- anthill_back/src/modules/transactions/data/dtos/transaction.stats.dto.ts
+++ anthill_back/src/modules/transactions/data/dtos/transaction.stats.dto.ts
@@ -1,8 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class TransactionStatsDto {
   fromDate: Date;
   toDate: Date;
-  sum: number;
-  average: number;
-  count: number;
-  largestDonation: number;
+  largestIncome: number;
+  averageIncome: number;
+  incomesSum: number;
+  incomesCount: number;
+  expensesCount: number;
+  expensesSum: number;
+
+  // a map from a Date string representation (YYYY-MM-DD)
+  // to total amount received at that day
+  @ApiProperty({
+    name: 'balances',
+    type: {},
+    example: {
+      '2024-03-18': 12.3,
+      '2024-03-17': 3.4,
+    },
+  })
+  balances: {
+    [key: string]: number;
+  };
 }
