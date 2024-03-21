3dc7ab5beabcbc5132fc52773ee444ac80a08ac6
Dmytro Popov <thedmitryp@ukr.net>
2024-03-21 14:35:29 +0000

    Fixed the `groupBy` function

--- anthill_back/src/common/utils/group-by.ts
+++ anthill_back/src/common/utils/group-by.ts
@@ -1,7 +1,8 @@
 export function groupBy<E>(arr: E[], by: (_: E) => string): { [key: string]: E[] } {
   return arr.reduce((resMap, elem) => {
     const key = by(elem);
-    (resMap[key] = resMap[elem[key]] || []).push(elem);
+    resMap[key] = resMap[key] ?? [];
+    resMap[key].push(elem);
     return resMap;
   }, {});
 }
