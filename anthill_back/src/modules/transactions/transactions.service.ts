ff83201a8706a074c9d878b15eb14b23437179a6
Dmytro Popov <thedmitryp@ukr.net>
2024-04-07 01:03:59 +0100

    Limited volunteers' controls over not-owned transactions

--- anthill_back/src/modules/transactions/transactions.service.ts
+++ anthill_back/src/modules/transactions/transactions.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectMapper } from 'automapper-nestjs';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Transaction } from './data/entities/transaction.entity';
@@ -16,6 +16,8 @@ import {
   PaginateQuery,
 } from 'nestjs-paginate';
 import { ReadManyDto } from '../../common/domain/read-many.dto';
+import { SessionPayloadDto } from '../auth/data/dtos/session.payload.dto';
+import { UserRole } from '../users/data/entities/user.entity';
 
 @Injectable()
 export class TransactionsService extends ModifiableResourceServiceBase<
@@ -49,6 +51,55 @@ export class TransactionsService extends ModifiableResourceServiceBase<
 
     return this.mapPaginated(paginated);
   }
+
+  async update(
+    id: number,
+    dto: TransactionUpdateDto,
+    currentUser?: SessionPayloadDto,
+  ): Promise<TransactionReadDto> {
+    const existing = await this.repository.findOne({ where: { id } });
+
+    if (!existing || existing.deleteDate !== null) {
+      throw new NotFoundException();
+    }
+
+    if (
+      currentUser &&
+      currentUser.id != existing.user.id &&
+      currentUser.role === UserRole.volunteer
+    ) {
+      throw new ForbiddenException(null, 'Current user does not own the accessed resource');
+    }
+
+    const entity = this.mapper.map(dto, this.updateDtoType, this.entityType);
+    const updated = await this.repository.save({ id, ...entity });
+
+    return this.mapOne(Object.assign(updated, existing));
+  }
+
+  async delete(id: number, currentUser?: SessionPayloadDto): Promise<boolean> {
+    const entity = await this.repository.findOne({
+      where: { id },
+      withDeleted: false,
+      relations: this.paginateConfig.relations,
+    });
+
+    if (entity === null) {
+      throw new NotFoundException();
+    }
+
+    if (
+      currentUser &&
+      currentUser.id != entity.user.id &&
+      currentUser.role === UserRole.volunteer
+    ) {
+      throw new ForbiddenException(null, 'Current user does not own the accessed resource');
+    }
+
+    await this.repository.softDelete({ id });
+
+    return true;
+  }
 }
 
 export const transactionsPaginateConfig: PaginateConfig<Transaction> = {
@@ -60,5 +111,4 @@ export const transactionsPaginateConfig: PaginateConfig<Transaction> = {
     deleteDate: [FilterOperator.NULL, FilterSuffix.NOT],
     createDate: [FilterOperator.BTW],
   },
-  // todo filters
 };
